f5dfae248cba20cf1a7b009a9b64cc46186a074f
user <user@host.docker.internal>
2021-01-29 13:05:35 +0900

    Demo2 の修正

--- demo2/src/index.jsx
+++ demo2/src/index.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 import {render} from 'react-dom';
 
 class App extends React.Component {
-  render () {
-    return <p> Hello React!</p>;
-  }
+
+	constructor(props) { 
+		super(props)
+		this.state = { text1: 'インドゾウ' }
+	}
+
+	onChange(e) {
+		 this.setState( {text1: e.target.value} )
+	}
+
+	render () {
+		return (
+			<div>
+				<input type="text" onChange = { this.onChange.bind(this) } />
+				<p>{ this.state.text1 }</p>
+			</div>
+		);
+	}
 }
 
-render(<App/>, document.getElementById('app'));
+render(<App/>, document.getElementById('react_app1'));
--- demo2/webpack.config.js
+++ demo2/webpack.config.js
@@ -1,2 +1,29 @@
-require('@babel/register'); // development.jsでES6を使えるようにする
-module.exports = require('./development');
+
+const src  = __dirname + '/src';
+const dist = __dirname + '/dist';
+
+module.exports = {
+  mode: 'development',
+  entry: src + '/index.jsx',
+
+  output: {
+    path: dist,
+    filename: 'r_app1.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  plugins: []
+};
